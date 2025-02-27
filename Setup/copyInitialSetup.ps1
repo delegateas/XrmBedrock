57de3ebdbbe31fd4bc98eeee6c3fdd0ca2e1545b
Magnus Gether Sørensen <mso@delegate.dk>
2025-02-27 09:14:30 +0100

    Update .gitignore, scripts, solution, and TypeScript definitions
    - Updated .gitignore to include/exclude specific paths and files.
    - Modified copyInitialSetup.ps1 to add new Copy-Item commands for initial setup.
    - Changed project GUID in XrmBedrock.sln for project {878E76AC-232E-491E-B669-AAA2623F4BED} to {2C2DA732-CDC7-4BFB-A544-FC26B8A1C163}.
    - Removed entire namespace Form.mgs_invoicecollection.Main from Information.d.ts.

---
+++ Setup/copyInitialSetup.ps1
@@ -0,0 +1,3 @@
+Copy-Item -Path "Setup/InitialSetup/WebResources" -Destination "src/Dataverse" -Recurse -Force
+Copy-Item -Path "Setup/InitialSetup/SharedTest" -Destination "src/Dataverse" -Recurse -Force
+Copy-Item -Path "Setup/InitialSetup/SharedContext" -Destination "src/Shared" -Recurse -Force
