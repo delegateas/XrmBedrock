77c3dc0985f1c65ceeb2ff1a7aece509b678d19f
Magnus Gether Sørensen <mso@delegate.dk>
2025-02-28 12:55:35 +0100

    Updated gitignore

--- src/Dataverse/WebResources/typings/XRM/_internal/Enum/mgs_billinginterval.d.ts
+++
@@ -1,4 +0,0 @@
-declare const enum mgs_billinginterval {
-  Yearly = 862810000,
-  Monthly = 862810001,
-}
--- src/Dataverse/WebResources/typings/XRM/_internal/Enum/mgs_product_statecode.d.ts
+++
@@ -1,4 +0,0 @@
-declare const enum mgs_product_statecode {
-  Active = 0,
-  Inactive = 1,
-}
