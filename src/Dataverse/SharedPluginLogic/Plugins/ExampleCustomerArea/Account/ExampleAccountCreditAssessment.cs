5ea64e51065e9076f3aefbafab7d82cb12dd3eb9
Jacob Blom Lillethorup <jba@delegate.dk>
2025-06-26 11:20:45 +0200

    Removed a space that pissed of style-cop

--- src/Dataverse/SharedPluginLogic/Plugins/ExampleCustomerArea/Account/ExampleAccountCreditAssessment.cs
+++ src/Dataverse/SharedPluginLogic/Plugins/ExampleCustomerArea/Account/ExampleAccountCreditAssessment.cs
@@ -7,7 +7,7 @@ namespace Dataverse.Plugins.CustomerArea;
 /// <summary>
 /// This plugin is purely for demonstration purposes and should be removed from the solution, when you have created a few real plugins in project.
 /// This plugin gives a simple demonstration of adding a reated object (here a Task) on creation of an object (here an Account).
-/// 
+///
 /// A good practice is to describe what a plugin does in natuaral languate. For this plugin it would be something like:
 /// This plugin creates a Task for Credit Assessment when an Account is created with Customer Type set to "Supplier".
 /// </summary>
