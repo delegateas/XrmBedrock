b04fadb459f21976069f95b955397799d6d887c0
Magnus Gether Sørensen <mso@delegate.dk>
2025-08-18 14:38:11 +0200

    Make Parameters property writable in ExecuteRequest
    Changed the `Parameters` property in the `ExecuteRequest` class from read-only to read-write. This modification allows for the `Parameters` dictionary to be modified after the object is instantiated.

--- test/XrmMockupApi/Models/ExecuteRequest.cs
+++ test/XrmMockupApi/Models/ExecuteRequest.cs
@@ -4,5 +4,5 @@ public class ExecuteRequest
 {
     public string RequestName { get; set; } = string.Empty;
 
-    public Dictionary<string, object> Parameters { get; } = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+    public Dictionary<string, object> Parameters { get; set; } = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
 }
