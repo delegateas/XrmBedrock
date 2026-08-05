25600de559b385810b11747e63be26c884f4e0a9
Jacob Blom Lillethorup <jba@delegate.dk>
2026-08-05 14:41:01 +0200

    Adjustments after fixing the example-azure-branch

--- test/Tests/XrmMockupFixture.cs
+++ test/Tests/XrmMockupFixture.cs
@@ -26,7 +26,7 @@ public class XrmMockupFixture
                     BaseCustomApiTypes = new Tuple<string, Type>[] { new("templatepublisherprefix", typeof(CustomAPI)) },
                     EnableProxyTypes = true,
                     IncludeAllWorkflows = false,
-                    MetadataDirectoryPath = Path.Combine("..", "..", "..", "..", "SharedTest", "MetadataGenerated"),
+                    MetadataDirectoryPath = Path.Combine("..", "..", "..", "..", "Tests", "MetadataGenerated"),
                 };
             }
         }
